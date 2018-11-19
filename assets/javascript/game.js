b1dbe9d3cdbe356a7e0d672a1eeba8c2b527d6bf
Lily <44213621+lilysimmons@users.noreply.github.com>
2018-11-19 17:03:17 -0500

    functional game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,15 @@
-// Creates an array that lists out all of the options (Rock, Paper, or Scissors).
-var parrotType = ["macaw", "cockatoo", "budgerigar", "lorikeet", "amazon"];
-// var randomParrot = parrotType[Math.floor(Math.random() * parrotType.length)].split("")
-// console.log(parrotType);
-// var hiddenWord = []
 
-// for(var i = 0; i<randomParrot.length; i++){
-//     hiddenWord.push("_")
-// }
-// console.log(hiddenWord)
+// global variables
+var parrotType = ["macaw", "cockatoo", "budgerigar", "lorikeet", "amazon", "kakapo", "rosella"];
 
-// var incorrectLetter = ["f", "h", "j", "p", "q", "s", "v", "x", "y"];
-
-// var correctLetter = ["m", "o", "b", "g", "l", "n", "a", "k", "u","e","i","c","t","d","r","w","z"];
-
-// Creating variables to hold the number of wins, losses, and ties. They start at 0.
-var wins;
-var losses;
+var wins=0;
+var losses=0;
 var guesses;
+var randomParrot;
+var hiddenWord;
+var wrongLetterChoice;
 
-var randomParrot
 
-var hiddenWord 
-
-var wrongLetterChoice
-
-// var letterGuessed = [];
-
-// var randomParrot = parrotType[Math.floor(Math.random() * parrotType.length)].split(" ");
 
 
 // Create variables that hold references to the places in the HTML where we want to display things.
@@ -42,14 +25,14 @@ var startGame = document.getElementsByClassName("btn btn-primary btn-lg");
  
 
 var resetGame = function(){
-   wins = 0;
-   losses = 0;
+  //  wins = 0;
+  //  losses = 0;
    guesses = 12;
+   // Randomly chooses a choice from the options array. 
    randomParrot = parrotType[Math.floor(Math.random() * parrotType.length)].split("");
    console.log(randomParrot);
     hiddenWord = [];
     wrongLetterChoice = [];
-    console.log(wrongLetterChoice);
     for(var i = 0; i<randomParrot.length; i++){
       hiddenWord.push("_");}
 
@@ -62,10 +45,11 @@ var resetGame = function(){
       winsText.textContent = "Wins: " + wins;
       
       lossesText.textContent = "Losses:" + losses;  
+      
 }
-
-
 startGame.onclick = resetGame();
+
+
     
 
   
@@ -99,33 +83,18 @@ for(var j = 0; j<randomParrot.length; j++){
     
     
 if (!hiddenWord.includes("_") && guesses>=0){
-    wins++;}
+    wins++;
+    alert("You guessed correctly! The hidden word was " + randomParrot.join("") + "!");
+    resetGame();
+  }
     
 else if (hiddenWord.includes("_") && guesses===0){
   losses++;
+  alert("You lose! Please try again.");
+  resetGame();
 }
 
 
-
-
-
-// for(var i = 0; i<correctLetter.length; i++){
-//   letterGuessed.push([i]);
-//   console.log(letterGuessed);
-
-
-//   if (correctLetter[i] === userGuess){
-//       letterguessed[i] = userGuess;
-//       // console.log(hiddenWord);
-//   }
-//   else if (correctLetter[i] !== userGuess) {
-//     letterGuessed.push([i]);
-//     console.log(letterGuessed);
-//   }
- 
-    
-//   }
-    
 guessesLeft.textContent = "Guesses left: " + guesses;  
 
 displayWord.textContent = "Word: " + hiddenWord.join(" ");
@@ -136,11 +105,10 @@ winsText.textContent = "Wins: " + wins;
 
 lossesText.textContent = "Losses:" + losses;
 
+};
 
-}
 
 
-// alreadyGuessed.textContent = " " + letterGuessed.join(" ");
 
 
 
@@ -150,36 +118,7 @@ lossesText.textContent = "Losses:" + losses;
  
 
 
-  // Randomly chooses a choice from the options array. This is the Computer's guess.
-  // var  = parrotType[Math.floor(Math.random() * parrotType.length)];
   
-  
-
-  // Only run the following code block if the user presses "r" or "p" or "s".
-  // if ((userGuess === "m") || (userGuess === "a") || (userGuess === "c") 
-  //     || (userGuess === "o") || (userGuess === "k") || (userGuess === "t")
-  //     || (userGuess === "b") || (userGuess === "u") || (userGuess === "d")
-  //     || (userGuess === "g") || (userGuess === "e") || (userGuess === "r")
-  //     || (userGuess === "l") || (userGuess === "i") || (userGuess === "z") 
-  //     || (userGuess === "n") || (userGuess === "w")){
-
-  //       hiddenWord.textContent = "_ " + userGuess;
-          
-  //     }
-
-
-        
-    // hiddenWord.textcontent = "you chose:" + userGuess;
-    
-     
- 
-      // else if ((userGuess === "f") || (userGuess === "h") || (userGuess === "j")
-      // || (userGuess === "p") || (userGuess === "q") || (userGuess === "s")
-      // || (userGuess === "v") || (userGuess === "x") || (userGuess === "y")){
-
-      //   alreadyGuessed.textContent = "Already Guessed: " + userGuess;
-      //   guessesLeft.textContent = "Guesses Left: " + guessesLeft--;
-      // }
 
     
   
@@ -192,24 +131,6 @@ lossesText.textContent = "Losses:" + losses;
 
 
 
-
-      // userChoiceText.textContent = "You chose: " + userGuess;
- 
-  // event.textContent = "You chose: " + letter;
-  //       console.log("this worked")
- 
-
-    // // Hide the directions
-    // directionsText.textContent = "";
-
-    // Display the user and computer guesses, and wins/losses/ties.
-    
-    
-    
-    
-    // newWordText.textContent = "The computer chose: " + newWord;
-    // winsText.textContent = "wins: " + wins;
-    // lossesText.textContent = "losses: " + losses;
   
   
 
